77d551dbad867f6d0b5182c07d1c07fc7430aeab
StellaKw <pstellak@my.centennialcollege.ca>
2025-04-07 12:38:47 -0400

    first commit

---
+++ paul_quiz4_donet/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace paul_quiz4_donet.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Message"] = "Database access has already been done on April 1, 2025 by Paule Leslie Stella Kwate";
+        }
+
+    }
+}
